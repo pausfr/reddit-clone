3c4958d7fdc056a0580a3ce36a1bfd8b4b7f9846
pausfr <lost7l@icloud.com>
2024-01-29 22:56:50 +0900

    월요 스터디 240129

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,10 +1,11 @@
 import React, { useState, FormEvent } from "react";
-import InputGroup from "@/src/components/InputGroup";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import InputGroup from "../components/InputGroup";
 import axios from "axios";
 
-const Login = () => {
+const Register = () => {
+  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
@@ -14,14 +15,13 @@ const Login = () => {
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
-      await axios.post(
-        "/auth/login",
-        {
-          password,
-          username,
-        },
-        { withCredentials: true }
-      );
+      const res = await axios.post("/auth/register", {
+        email,
+        password,
+        username,
+      });
+      console.log(res);
+      router.push("/login");
     } catch (error: any) {
       console.log("error", error);
       setErrors(error.response.data || {});
@@ -32,8 +32,14 @@ const Login = () => {
     <div className="bg-white text-black">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
-          <h1 className="mb-2 text-lg font-medium">로그인</h1>
+          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
           <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
             <InputGroup
               placeholder="Username"
               value={username}
@@ -47,13 +53,13 @@ const Login = () => {
               error={errors.password}
             />
             <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              로그인
+              회원 가입
             </button>
           </form>
           <small>
-            아직 아이디가 없나요?
-            <Link legacyBehavior href="/register">
-              <a className="ml-1 text-blue-500 uppercase">회원가입</a>
+            이미 가입하셨나요?
+            <Link legacyBehavior href="/login">
+              <a className="ml-1 text-blue-500 uppercase">로그인</a>
             </Link>
           </small>
         </div>
@@ -62,4 +68,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
